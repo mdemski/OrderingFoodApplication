616358b57d8ef452163faecfe2365f5395474a99
mdemski <m.demski@outlook.com>
2019-09-19 18:24:51 +0200

    User service

---
+++ src/main/java/pl/mdemski/services/UserService.java
@@ -0,0 +1,23 @@
+package pl.mdemski.services;
+
+import org.springframework.stereotype.Service;
+import pl.mdemski.model.User;
+import pl.mdemski.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.getByEmail(email);
+    }
+
+}
