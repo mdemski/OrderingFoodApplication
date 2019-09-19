1e58e9df3d9eb843f8e12d4621483d5baa67a8a6
mdemski <m.demski@outlook.com>
2019-09-19 20:15:41 +0200

    Fix UserRepository

--- src/main/java/pl/mdemski/repositories/UserRepository.java
+++ src/main/java/pl/mdemski/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package pl.mdemski.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
 import pl.mdemski.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<User, Long> {
 
     long countByEmail(String email);
 
