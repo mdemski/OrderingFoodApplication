09ef15d7c08801f6951c8741155074a9a4c47d05
mdemski <m.demski@outlook.com>
2019-09-23 21:17:07 +0200

    Added annotations too the fields

--- src/main/java/pl/mdemski/dto/RegistrationFormDTO.java
+++ src/main/java/pl/mdemski/dto/RegistrationFormDTO.java
@@ -2,7 +2,10 @@ package pl.mdemski.dto;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.pl.NIP;
+import pl.mdemski.validation.PostCode;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Size;
 
 public class RegistrationFormDTO {
@@ -18,6 +21,87 @@ public class RegistrationFormDTO {
     private String rePassword;
     @NotBlank
     private String firstName;
+    @NotBlank
+    private String companyName;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String address;
+    @NotBlank
+    @PostCode
+    private String postCode;
+    @NIP
+    private String companyNip;
+    @NotBlank
+    private String contactFirstName;
+    @NotBlank
+    private String contactLastName;
+    @AssertTrue(message = "Brak akceptacji regualminu.")
+    private boolean statuteKnowledge;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCompanyNip() {
+        return companyNip;
+    }
+
+    public void setCompanyNip(String companyNip) {
+        this.companyNip = companyNip;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public boolean isStatuteKnowledge() {
+        return statuteKnowledge;
+    }
+
+    public void setStatuteKnowledge(boolean statuteKnowledge) {
+        this.statuteKnowledge = statuteKnowledge;
+    }
 
     public String getEmail() {
         return email;
