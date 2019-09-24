12b9a96052667df699c2e41e4da911c2294d4c0c
mdemski <m.demski@outlook.com>
2019-09-24 20:05:09 +0200

    Added CompanyDTO class

---
+++ src/main/java/pl/mdemski/dto/CompanyDTO.java
@@ -0,0 +1,77 @@
+package pl.mdemski.dto;
+
+public class CompanyDTO {
+
+    private Long id;
+    private String companyName;
+    private String city;
+    private String address;
+    private String postCode;
+    private String companyNip;
+    private String contactFirstName;
+    private String contactLastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCompanyNip() {
+        return companyNip;
+    }
+
+    public void setCompanyNip(String companyNip) {
+        this.companyNip = companyNip;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+}
