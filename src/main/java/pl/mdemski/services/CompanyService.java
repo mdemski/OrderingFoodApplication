95b6b4441360353e865cfe1168a11f2d1b470e80
mdemski <m.demski@outlook.com>
2019-09-24 20:06:20 +0200

    Initialize CompanyService with getting all companies method

---
+++ src/main/java/pl/mdemski/services/CompanyService.java
@@ -0,0 +1,41 @@
+package pl.mdemski.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import pl.mdemski.dto.CompanyDTO;
+import pl.mdemski.model.Company;
+import pl.mdemski.repositories.CompanyRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<CompanyDTO> getAllCompanies(){
+        Page<Company> companyDTOS = companyRepository.findAll(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "created")));
+        List<Company> content = companyDTOS.getContent();
+        return content.stream().map(source -> {
+            CompanyDTO dto = new CompanyDTO();
+            dto.setId(source.getId());
+            dto.setCompanyName(source.getName());
+            dto.setCity(source.getCity());
+            dto.setAddress(source.getAddress());
+            dto.setPostCode(source.getPostCode());
+            dto.setCompanyNip(source.getNip());
+            dto.setContactFirstName(source.getContactFirstName());
+            dto.setContactLastName(source.getContactLastName());
+            return dto;
+        }).collect(Collectors.toList());
+    }
+}
