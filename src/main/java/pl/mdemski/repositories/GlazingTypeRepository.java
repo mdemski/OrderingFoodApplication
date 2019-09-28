8e3d6fc53fea3aa72f91e04e213f66ab6f7a0e2b
mdemski <m.demski@outlook.com>
2019-09-28 15:51:54 +0200

    Glazing type repository

---
+++ src/main/java/pl/mdemski/repositories/GlazingTypeRepository.java
@@ -0,0 +1,9 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.GlazingType;
+
+public interface GlazingTypeRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<GlazingType, Long> {
+}
+
