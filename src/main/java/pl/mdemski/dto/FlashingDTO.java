7a9fa9c8f20de008811a7762eec57eaadee91a13
mdemski <m.demski@outlook.com>
2019-09-28 16:28:13 +0200

    Flashing dto class

---
+++ src/main/java/pl/mdemski/dto/FlashingDTO.java
@@ -0,0 +1,13 @@
+package pl.mdemski.dto;
+
+public class FlashingDTO {
+    private String typeName;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
