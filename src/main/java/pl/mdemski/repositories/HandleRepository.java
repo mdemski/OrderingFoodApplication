da367bfc259f58efb64aa543d344a6b02ae944e6
mdemski <m.demski@outlook.com>
2019-09-28 16:19:49 +0200

    Handle repository

---
+++ src/main/java/pl/mdemski/repositories/HandleRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.Handle;
+
+public interface HandleRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Handle, Long> {
+}
