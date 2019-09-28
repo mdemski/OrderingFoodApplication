871f26ec45dc72bb20fc53de0ecbdb5bb2014f37
mdemski <m.demski@outlook.com>
2019-09-28 14:50:13 +0200

    Window repository

---
+++ src/main/java/pl/mdemski/repositories/WindowRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.Window;
+
+public interface WindowRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Window, Long> {
+}
