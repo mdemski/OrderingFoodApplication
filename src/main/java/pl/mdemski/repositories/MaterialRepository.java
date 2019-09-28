6e38c5c0d41575352c87c0a6e0ecb7062fa33a98
mdemski <m.demski@outlook.com>
2019-09-28 14:49:18 +0200

    Material repository

---
+++ src/main/java/pl/mdemski/repositories/MaterialRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.Material;
+
+public interface MaterialRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Material, Long> {
+}
