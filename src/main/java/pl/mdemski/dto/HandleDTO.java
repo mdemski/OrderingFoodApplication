7b8b5ef2e7b5fd7ac96ca7ae877eefc66a79bba4
mdemski <m.demski@outlook.com>
2019-09-28 16:18:58 +0200

    Handle dto class

---
+++ src/main/java/pl/mdemski/dto/HandleDTO.java
@@ -0,0 +1,32 @@
+package pl.mdemski.dto;
+
+public class HandleDTO {
+
+    private Long id;
+    private String color;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
