2082c4350b15732248b96bcdfadbcb4d46c96aef
mdemski <m.demski@outlook.com>
2019-09-28 16:08:16 +0200

    Material color repository

---
+++ src/main/java/pl/mdemski/repositories/MaterialColorRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.MaterialColor;
+
+public interface MaterialColorRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<MaterialColor, Long> {
+}
