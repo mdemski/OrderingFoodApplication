9b560f73c033d88ec9187f51653d716a9b7aa460
mdemski <m.demski@outlook.com>
2019-09-28 14:51:19 +0200

    Opening type service

---
+++ src/main/java/pl/mdemski/services/OpeningTypeService.java
@@ -0,0 +1,42 @@
+package pl.mdemski.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import pl.mdemski.dto.OpeningTypeDTO;
+import pl.mdemski.model.OpeningType;
+import pl.mdemski.repositories.OpeningTypeRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OpeningTypeService {
+
+    private OpeningTypeRepository openingTypeRepository;
+
+    public OpeningTypeService(OpeningTypeRepository openingTypeRepository) {
+        this.openingTypeRepository = openingTypeRepository;
+    }
+
+    public void addNewOpeningType(){
+        OpeningType openingType = new OpeningType("obrotowe");
+        OpeningType openingType1 = new OpeningType("uchylno-przesuwne");
+        OpeningType openingType2 = new OpeningType("wysokoosiowe");
+        OpeningType openingType3 = new OpeningType("nieotwieralne");
+        openingTypeRepository.save(openingType);
+        openingTypeRepository.save(openingType1);
+        openingTypeRepository.save(openingType2);
+        openingTypeRepository.save(openingType3);
+    }
+
+    public List<OpeningTypeDTO> getAllOpeningTypes(){
+        Page<OpeningType> openingTypes = openingTypeRepository.findAll(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "id")));
+        List<OpeningType> content = openingTypes.getContent();
+        return content.stream().map(source -> {
+            OpeningTypeDTO dto = new OpeningTypeDTO();
+            dto.setId(source.getId());
+            dto.setName(source.getName());
+            return dto;
+        }).collect(Collectors.toList());
+    }
+}
