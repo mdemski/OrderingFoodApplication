7a5e9aba5a688d46d2b2d29162e0b36af5745c96
mdemski <m.demski@outlook.com>
2019-09-28 16:27:24 +0200

    Flashing repository

---
+++ src/main/java/pl/mdemski/repositories/FlashingRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.Flashing;
+
+public interface FlashingRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Flashing, Long> {
+}
