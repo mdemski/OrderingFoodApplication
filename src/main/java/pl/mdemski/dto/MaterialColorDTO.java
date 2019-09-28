6e8cce6b48fe31bb9797fcf0d39c36677859abb9
mdemski <m.demski@outlook.com>
2019-09-28 15:50:45 +0200

    Material color dto class

---
+++ src/main/java/pl/mdemski/dto/MaterialColorDTO.java
@@ -0,0 +1,13 @@
+package pl.mdemski.dto;
+
+public class MaterialColorDTO {
+    private String ralName;
+
+    public String getRalName() {
+        return ralName;
+    }
+
+    public void setRalName(String ralName) {
+        this.ralName = ralName;
+    }
+}
