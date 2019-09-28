203465f1d411d1e5afae1f63848e62bc955e23f3
mdemski <m.demski@outlook.com>
2019-09-28 14:49:56 +0200

    Opening type repository

---
+++ src/main/java/pl/mdemski/repositories/OpeningTypeRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.OpeningType;
+
+public interface OpeningTypeRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<OpeningType, Long> {
+}
