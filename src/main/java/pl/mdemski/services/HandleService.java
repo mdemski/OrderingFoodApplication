d1422ef549555506a91196057cdb4d9ee315982c
mdemski <m.demski@outlook.com>
2019-09-28 16:20:10 +0200

    Handle service class

---
+++ src/main/java/pl/mdemski/services/HandleService.java
@@ -0,0 +1,47 @@
+package pl.mdemski.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import pl.mdemski.dto.HandleDTO;
+import pl.mdemski.model.Handle;
+import pl.mdemski.repositories.HandleRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class HandleService {
+
+    private HandleRepository handleRepository;
+
+    public HandleService(HandleRepository handleRepository) {
+        this.handleRepository = handleRepository;
+    }
+
+    public void addHandle(){
+        Handle handle = new Handle("biała", "z kluczykiem");
+        Handle handle1 = new Handle("szampańska", "z kluczykiem");
+        Handle handle2 = new Handle("biała", "bez kluczyka");
+        Handle handle3 = new Handle("szampańska", "bez kluczyka");
+        handleRepository.save(handle);
+        handleRepository.save(handle1);
+        handleRepository.save(handle2);
+        handleRepository.save(handle3);
+    }
+
+    public List<HandleDTO> getAllGlazingTypes(){
+        Page<Handle> glazingType = handleRepository.findAll(new PageRequest(0, 20, new Sort(Sort.Direction.DESC, "id")));
+        List<Handle> content = glazingType.getContent();
+        return content.stream().map(source -> {
+            HandleDTO dto = new HandleDTO();
+            dto.setId(source.getId());
+            dto.setColor(source.getColor());
+            dto.setType(source.getType());
+            return dto;
+        }).collect(Collectors.toList());
+    }
+}
