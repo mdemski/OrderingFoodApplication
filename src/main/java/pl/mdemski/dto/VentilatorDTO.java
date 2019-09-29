b8012ca7eedb7a1e10676c8d53b09954815546af
mdemski <m.demski@outlook.com>
2019-09-29 09:37:44 +0200

    Ventilator dto class

---
+++ src/main/java/pl/mdemski/dto/VentilatorDTO.java
@@ -0,0 +1,14 @@
+package pl.mdemski.dto;
+
+public class VentilatorDTO {
+
+    private String ventiName;
+
+    public String getVentiName() {
+        return ventiName;
+    }
+
+    public void setVentiName(String ventiName) {
+        this.ventiName = ventiName;
+    }
+}
