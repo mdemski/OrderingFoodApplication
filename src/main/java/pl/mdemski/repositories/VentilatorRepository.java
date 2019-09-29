b4d94ecd1d21ce171cf3c17998002377c7ef3c78
mdemski <m.demski@outlook.com>
2019-09-29 09:37:22 +0200

    Ventilator repository

---
+++ src/main/java/pl/mdemski/repositories/VentilatorRepository.java
@@ -0,0 +1,8 @@
+package pl.mdemski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mdemski.model.AbstractEntity;
+import pl.mdemski.model.Ventilator;
+
+public interface VentilatorRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Ventilator, Long> {
+}
