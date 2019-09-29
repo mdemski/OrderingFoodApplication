6969dc2f382beab2b46420c8b39a80ed9a78091a
mdemski <m.demski@outlook.com>
2019-09-29 09:12:09 +0200

    Added id field

--- src/main/java/pl/mdemski/dto/FlashingDTO.java
+++ src/main/java/pl/mdemski/dto/FlashingDTO.java
@@ -1,7 +1,16 @@
 package pl.mdemski.dto;
 
 public class FlashingDTO {
+    private Long id;
     private String typeName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTypeName() {
         return typeName;
