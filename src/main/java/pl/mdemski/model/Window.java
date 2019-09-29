472c02a067e67d57274910a9e983001e4d8a0ede
mdemski <m.demski@outlook.com>
2019-09-29 09:19:24 +0200

    Changed fields describing window and added relation between window and another entities

--- src/main/java/pl/mdemski/model/Window.java
+++ src/main/java/pl/mdemski/model/Window.java
@@ -2,7 +2,6 @@ package pl.mdemski.model;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,13 +13,112 @@ public class Window extends AbstractEntity {
     private Material material;
     @ManyToOne
     private OpeningType openingType;
-    private String dimensions; //rozbić to na wysokość i szerokość
-    private String glazingType; //stworzyć osobną encję
-    private String materialColor; //stworzyć osobną encję
-    private String handle; //stworzyć osobną encję
+    private double height;
+    private double width;
+    @ManyToOne
+    private GlazingType glazingType;
+    @ManyToOne
+    private MaterialColor materialColor;
+    @ManyToOne
+    private String handle;
     @ManyToOne
     private Flashing flashing;
-    private String ventilator; //stowrzyć osobną encję
+    @ManyToOne
+    private Ventilator ventilator;
 
+    public Window(MountingAngle mountingAngle, Material material, OpeningType openingType, double height, double width, GlazingType glazingType, MaterialColor materialColor, String handle, Flashing flashing, Ventilator ventilator) {
+        this.mountingAngle = mountingAngle;
+        this.material = material;
+        this.openingType = openingType;
+        this.height = height;
+        this.width = width;
+        this.glazingType = glazingType;
+        this.materialColor = materialColor;
+        this.handle = handle;
+        this.flashing = flashing;
+        this.ventilator = ventilator;
+    }
 
+    public Window() {
+    }
+
+    public MountingAngle getMountingAngle() {
+        return mountingAngle;
+    }
+
+    public void setMountingAngle(MountingAngle mountingAngle) {
+        this.mountingAngle = mountingAngle;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public OpeningType getOpeningType() {
+        return openingType;
+    }
+
+    public void setOpeningType(OpeningType openingType) {
+        this.openingType = openingType;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public GlazingType getGlazingType() {
+        return glazingType;
+    }
+
+    public void setGlazingType(GlazingType glazingType) {
+        this.glazingType = glazingType;
+    }
+
+    public MaterialColor getMaterialColor() {
+        return materialColor;
+    }
+
+    public void setMaterialColor(MaterialColor materialColor) {
+        this.materialColor = materialColor;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public Flashing getFlashing() {
+        return flashing;
+    }
+
+    public void setFlashing(Flashing flashing) {
+        this.flashing = flashing;
+    }
+
+    public Ventilator getVentilator() {
+        return ventilator;
+    }
+
+    public void setVentilator(Ventilator ventilator) {
+        this.ventilator = ventilator;
+    }
 }
