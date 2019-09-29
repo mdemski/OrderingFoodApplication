c4e9e56f757e6d904a14a5cbe79a5aa9a7a482ed
mdemski <m.demski@outlook.com>
2019-09-29 09:41:31 +0200

    Id filed added

--- src/main/java/pl/mdemski/dto/VentilatorDTO.java
+++ src/main/java/pl/mdemski/dto/VentilatorDTO.java
@@ -2,7 +2,16 @@ package pl.mdemski.dto;
 
 public class VentilatorDTO {
 
+    private Long id;
     private String ventiName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getVentiName() {
         return ventiName;
