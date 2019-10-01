1c3ae875649a81a0c45346d60218a92191a5b108
mdemski <m.demski@outlook.com>
2019-10-01 17:38:53 +0200

    Small fix

--- src/main/java/pl/mdemski/dto/MaterialColorDTO.java
+++ src/main/java/pl/mdemski/dto/MaterialColorDTO.java
@@ -12,11 +12,11 @@ public class MaterialColorDTO {
         this.ralName = ralName;
     }
 
-    public void setId(Long id) {
-
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
