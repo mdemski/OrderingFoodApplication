275b0d36b1d8b745eac18a15e7bdaf7533bef118
mdemski <m.demski@outlook.com>
2019-10-01 21:09:07 +0200

    Added some step in configuration. Added summary page controller

--- src/main/java/pl/mdemski/web/controllers/WindowConfiguratorController.java
+++ src/main/java/pl/mdemski/web/controllers/WindowConfiguratorController.java
@@ -2,18 +2,13 @@ package pl.mdemski.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import pl.mdemski.dto.MaterialDTO;
-import pl.mdemski.dto.MountingAngleDTO;
-import pl.mdemski.dto.OpeningTypeDTO;
-import pl.mdemski.dto.WindowDTO;
-import pl.mdemski.services.MaterialService;
-import pl.mdemski.services.MountingAngleService;
-import pl.mdemski.services.OpeningTypeService;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.mdemski.dto.*;
+import pl.mdemski.model.Window;
+import pl.mdemski.services.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -23,29 +18,63 @@ public class WindowConfiguratorController {
     private MountingAngleService mountingAngleService;
     private MaterialService materialService;
     private OpeningTypeService openingTypeService;
+    private GlazingTypeService glazingTypeService;
+    private MaterialColorService materialColorService;
+    private HandleService handleService;
+    private FlashingService flashingService;
+    private VentilatorService ventilatorService;
+    private WindowService windowService;
 
-    public WindowConfiguratorController(MountingAngleService mountingAngleService, MaterialService materialService, OpeningTypeService openingTypeService) {
+    public WindowConfiguratorController(MountingAngleService mountingAngleService, MaterialService materialService, OpeningTypeService openingTypeService, GlazingTypeService glazingTypeService, MaterialColorService materialColorService, HandleService handleService, FlashingService flashingService, VentilatorService ventilatorService, WindowService windowService) {
         this.mountingAngleService = mountingAngleService;
         this.materialService = materialService;
         this.openingTypeService = openingTypeService;
+        this.glazingTypeService = glazingTypeService;
+        this.materialColorService = materialColorService;
+        this.handleService = handleService;
+        this.flashingService = flashingService;
+        this.ventilatorService = ventilatorService;
+        this.windowService = windowService;
     }
 
     @ModelAttribute("mountingList")
     public List<MountingAngleDTO> mountingAngle() {
-        mountingAngleService.addNewMountingAngle();
         return mountingAngleService.getAllMountings();
     }
 
     @ModelAttribute("materials")
-    public List<MaterialDTO> material(){
-        materialService.addNewMaterial();
+    public List<MaterialDTO> material() {
         return materialService.getAllMaterials();
     }
 
     @ModelAttribute("openingTypes")
-    public List<OpeningTypeDTO> openingTypes(){
-        openingTypeService.addNewOpeningType();
+    public List<OpeningTypeDTO> openingTypes() {
         return openingTypeService.getAllOpeningTypes();
+    }
+
+    @ModelAttribute("glazingTypes")
+    public List<GlazingTypeDTO> glazingTypes() {
+        return glazingTypeService.getAllGlazingTypes();
+    }
+
+    @ModelAttribute("materialColors")
+    public List<MaterialColorDTO> materialColors() {
+        return materialColorService.getAllMaterialColors();
+    }
+
+    @ModelAttribute("handles")
+    public List<HandleDTO> handles() {
+        return handleService.getAllGlazingTypes();
+    }
+
+    @ModelAttribute("flashings")
+    public List<FlashingDTO> flashings() {
+        return flashingService.getAllFlashings();
+    }
+
+    @ModelAttribute("ventilators")
+    public List<VentilatorDTO> ventilators() {
+        return ventilatorService.getAllVentilators();
     }
 
     @GetMapping("/krok-1")
@@ -54,16 +83,133 @@ public class WindowConfiguratorController {
         return "firstStepWindowsConfig";
     }
 
-    @PostMapping("/krok-1")
-    public String processFirstStepWindowConfiguration(@ModelAttribute("data") WindowDTO windowDTO) {
-
-        windowDTO.getMountingAngle();
-        return "redirect:/krok-2";
+    @GetMapping("/krok-1/{id}")
+    public String prepareFirstStepWindowConfigurationWithId(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "firstStepWindowsConfig";
     }
 
-//    @GetMapping("/krok-2")
-//    public String prepareSecondStepWindowConfiguration(Model model) {
-//
-//        return "secondStepWindowsConfig";
-//    }
+    @PostMapping("/krok-1")
+    public String processFirstStepWindowConfiguration(WindowDTO windowDTO) {
+        Window window = windowService.addNewWindow(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-2/" + window.getId();
+    }
+
+    @GetMapping("/krok-2/{id}")
+    public String prepareSecondStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "secondStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-2")
+    public String processSecondStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-3/" + window.getId();
+    }
+
+    @GetMapping("/krok-3/{id}")
+    public String prepareThirdStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "thirdStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-3")
+    public String processThirdStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-4/" + window.getId();
+    }
+
+    @GetMapping("/krok-4/{id}")
+    public String prepareForthStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "forthStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-4")
+    public String processForthStepWindowConfiguration(@ModelAttribute("windowDTO") @Valid WindowDTO windowDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            return "forthStepWindowsConfig";
+        } else {
+            Window window = windowService.editWindowConfiguration(windowDTO);
+            return "redirect:/konfigurator/okna-dachowe/krok-5/" + window.getId();
+        }
+    }
+
+    @GetMapping("/krok-5/{id}")
+    public String prepareFifthStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "fifthStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-5")
+    public String processFifthStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-6/" + window.getId();
+    }
+
+    @GetMapping("/krok-6/{id}")
+    public String prepareSixthStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "sixthStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-6")
+    public String processSixthStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-7/" + window.getId();
+    }
+
+    @GetMapping("/krok-7/{id}")
+    public String prepareSeventhStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "seventhStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-7")
+    public String processSeventhStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-8/" + window.getId();
+    }
+
+    @GetMapping("/krok-8/{id}")
+    public String prepareEighthStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "eighthStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-8")
+    public String processEighthStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/krok-9/" + window.getId();
+    }
+
+    @GetMapping("/krok-9/{id}")
+    public String prepareNinthStepWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        return "ninthStepWindowsConfig";
+    }
+
+    @PostMapping("/krok-9")
+    public String processNinthStepWindowConfiguration(@ModelAttribute("windowDTO") WindowDTO windowDTO) {
+        Window window = windowService.editWindowConfiguration(windowDTO);
+        return "redirect:/konfigurator/okna-dachowe/podsumowanie/" + window.getId();
+    }
+
+    @GetMapping("/podsumowanie/{id}")
+    public String prepareSummaryWindowConfiguration(@PathVariable Long id, @ModelAttribute("windowDTO") WindowDTO windowDTO, Model model) {
+        model.addAttribute("windowDTO", windowService.getById(id));
+        WindowDTO windowDTO1 = windowService.getById(id);
+        model.addAttribute("chosenMountingAngle", mountingAngleService.getById(windowDTO1.getMountingAngleId()));
+        model.addAttribute("chosenMaterial", materialService.getById(windowDTO1.getMaterialId()));
+        model.addAttribute("chosenTypeOpening", openingTypeService.getById(windowDTO1.getOpeningTypeId()));
+        model.addAttribute("chosenWidth", windowDTO1.getWidth());
+        model.addAttribute("chosenHeight", windowDTO1.getHeight());
+        model.addAttribute("chosenGlazingType", glazingTypeService.getById(windowDTO1.getGlazingTypeId()));
+        model.addAttribute("chosenMaterialColor", materialColorService.getById(windowDTO1.getMaterialColorId()));
+        model.addAttribute("chosenHandle", handleService.getById(windowDTO1.getHandleId()));
+        model.addAttribute("chosenFlashing", flashingService.getById(windowDTO1.getFlashingNameId()));
+        model.addAttribute("chosenVentilator", ventilatorService.getById(windowDTO1.getVentilatorId()));
+        return "summaryWindowsConfig";
+    }
+
 }
