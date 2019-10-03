21efe48764ef4a54ee70adf0fc441e7e5344f451
mdemski <m.demski@outlook.com>
2019-10-03 20:42:18 +0200

    Move company registration to another DTO class

--- src/main/java/pl/mdemski/dto/RegistrationFormDTO.java
+++ src/main/java/pl/mdemski/dto/RegistrationFormDTO.java
@@ -21,79 +21,8 @@ public class RegistrationFormDTO {
     private String rePassword;
     @NotBlank
     private String firstName;
-    @NotBlank
-    private String companyName;
-    @NotBlank
-    private String city;
-    @NotBlank
-    private String address;
-    @NotBlank
-    @PostCode
-    private String postCode;
-    @NIP
-    private String companyNip;
-    @NotBlank
-    private String contactFirstName;
-    @NotBlank
-    private String contactLastName;
     @AssertTrue(message = "Brak akceptacji regualminu.")
     private boolean statuteKnowledge;
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    public String getCompanyNip() {
-        return companyNip;
-    }
-
-    public void setCompanyNip(String companyNip) {
-        this.companyNip = companyNip;
-    }
-
-    public String getContactFirstName() {
-        return contactFirstName;
-    }
-
-    public void setContactFirstName(String contactFirstName) {
-        this.contactFirstName = contactFirstName;
-    }
-
-    public String getContactLastName() {
-        return contactLastName;
-    }
-
-    public void setContactLastName(String contactLastName) {
-        this.contactLastName = contactLastName;
-    }
 
     public boolean isStatuteKnowledge() {
         return statuteKnowledge;
