2fe5609ca3c03943f21d194cb4e9bb658fe0beef
mdemski <m.demski@outlook.com>
2019-10-05 16:01:00 +0200

    My account page controller

---
+++ src/main/java/pl/mdemski/web/controllers/UserAccountController.java
@@ -0,0 +1,34 @@
+package pl.mdemski.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mdemski.model.User;
+import pl.mdemski.services.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/moje-konto")
+public class UserAccountController {
+
+    private UserService userService;
+
+    public UserAccountController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public String prepareUserInformationPage(@PathVariable Long id, Principal principal, Model model){
+        model.addAttribute("userId", id);
+        User user = userService.getUserByEmail(principal.getName());
+        if (!(user.getEmail().equals(userService.findById(id).getEmail()))){
+            return "index";
+        } else {
+            return "my-account";
+        }
+    }
+
+}
