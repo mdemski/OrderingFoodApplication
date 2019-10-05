f7ea100ecf34531546f9190cc36404ea8d190b20
mdemski <m.demski@outlook.com>
2019-10-05 12:55:34 +0200

    Added user and date to fields to get information how and when create configuration

--- src/main/java/pl/mdemski/model/Window.java
+++ src/main/java/pl/mdemski/model/Window.java
@@ -3,6 +3,8 @@ package pl.mdemski.model;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "windows")
@@ -25,8 +27,11 @@ public class Window extends AbstractEntity {
     private Flashing flashing;
     @ManyToOne
     private Ventilator ventilator;
+    @ManyToOne
+    private User user;
+    private LocalDateTime created;
 
-    public Window(MountingAngle mountingAngle, Material material, OpeningType openingType, int height, int width, GlazingType glazingType, MaterialColor materialColor, Handle handle, Flashing flashing, Ventilator ventilator) {
+    public Window(MountingAngle mountingAngle, Material material, OpeningType openingType, int height, int width, GlazingType glazingType, MaterialColor materialColor, Handle handle, Flashing flashing, Ventilator ventilator, User user, LocalDateTime created) {
         this.mountingAngle = mountingAngle;
         this.material = material;
         this.openingType = openingType;
@@ -37,6 +42,8 @@ public class Window extends AbstractEntity {
         this.handle = handle;
         this.flashing = flashing;
         this.ventilator = ventilator;
+        this.user = user;
+        this.created = created;
     }
 
     public Window() {
@@ -120,5 +127,47 @@ public class Window extends AbstractEntity {
 
     public void setVentilator(Ventilator ventilator) {
         this.ventilator = ventilator;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Window window = (Window) o;
+        return height == window.height &&
+                width == window.width &&
+                Objects.equals(mountingAngle, window.mountingAngle) &&
+                Objects.equals(material, window.material) &&
+                Objects.equals(openingType, window.openingType) &&
+                Objects.equals(glazingType, window.glazingType) &&
+                Objects.equals(materialColor, window.materialColor) &&
+                Objects.equals(handle, window.handle) &&
+                Objects.equals(flashing, window.flashing) &&
+                Objects.equals(ventilator, window.ventilator) &&
+                Objects.equals(user, window.user) &&
+                Objects.equals(created, window.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), mountingAngle, material, openingType, height, width, glazingType, materialColor, handle, flashing, ventilator, user, created);
     }
 }
