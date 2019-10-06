d476b5256accada484818e15637a8609ec2871f6
mdemski <m.demski@outlook.com>
2019-10-06 22:15:04 +0200

    Added new fields

--- src/main/java/pl/mdemski/services/MountingAngleService.java
+++ src/main/java/pl/mdemski/services/MountingAngleService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import pl.mdemski.dto.MountingAngleDTO;
+import pl.mdemski.model.FileConverter;
 import pl.mdemski.model.MountingAngle;
 import pl.mdemski.repositories.MountingAngleRepository;
 
@@ -23,10 +24,14 @@ public class MountingAngleService {
     }
 
     public void addNewMountingAngle() {
-        MountingAngle mountingAngle = new MountingAngle("02° - 15°");
-        MountingAngle mountingAngle1 = new MountingAngle("15° - 65°");
-        MountingAngle mountingAngle2 = new MountingAngle( "15° - 90°");
-        MountingAngle mountingAngle3 = new MountingAngle( "90°(pion)");
+        byte[] flat = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\2-15.png");
+        byte[] fifteenToNinety = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\15-90.png");
+        byte[] fifteenToSixtyF = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\15-65.png");
+        byte[] vertical = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\90(pion).png");
+        MountingAngle mountingAngle = new MountingAngle("02° - 15°", "Okna do dachów płaskich w nowoczesnych rozwiązaniach architektonicznych umożliwia dostarczenie dużej ilości światła.", flat);
+        MountingAngle mountingAngle1 = new MountingAngle("15° - 65°", "Wyróżnikiem okien uchylno-przesuwnych OKPOL jest wyjątkowa, umożliwiająca ich otwieranie do poziomu 60 stopni.", fifteenToSixtyF);
+        MountingAngle mountingAngle2 = new MountingAngle( "15° - 90°", " Okna obrotowe to popularna, rynkowa propozycja, stworzona dla prawdziwych miłośników wygody. ", fifteenToNinety);
+        MountingAngle mountingAngle3 = new MountingAngle( "90°(pion)", "Okna kolankowe OKPOL to designerski sposób na stworzenie jasnej i przestrzennej aranżacji poddasza.", vertical);
         mountingAngleRepository.save(mountingAngle);
         mountingAngleRepository.save(mountingAngle1);
         mountingAngleRepository.save(mountingAngle2);
