94dbc7c5de6227fbab609f49f1d1f34d0b907e15
mdemski <m.demski@outlook.com>
2019-10-06 21:50:30 +0200

    Added new fields, getters and setters and hashCode and equals methods

--- src/main/java/pl/mdemski/model/Handle.java
+++ src/main/java/pl/mdemski/model/Handle.java
@@ -2,6 +2,8 @@ package pl.mdemski.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
 @Table(name = "handles")
@@ -9,10 +11,14 @@ public class Handle extends AbstractEntity {
 
     private String color;
     private String type;
+    private String description;
+    private byte[] picture;
 
-    public Handle(String color, String type) {
+    public Handle(String color, String type, String description, byte[] picture) {
         this.color = color;
         this.type = type;
+        this.description = description;
+        this.picture = picture;
     }
 
     public Handle() {
@@ -32,5 +38,40 @@ public class Handle extends AbstractEntity {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Handle handle = (Handle) o;
+        return Objects.equals(color, handle.color) &&
+                Objects.equals(type, handle.type) &&
+                Objects.equals(description, handle.description) &&
+                Arrays.equals(picture, handle.picture);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(super.hashCode(), color, type, description);
+        result = 31 * result + Arrays.hashCode(picture);
+        return result;
     }
 }
