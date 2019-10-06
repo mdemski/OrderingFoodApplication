7b274d1f2a397f3c55153cf30a11afc6b1049b95
mdemski <m.demski@outlook.com>
2019-10-06 20:26:00 +0200

    New fields, getters and setters, hashCode and equals methods

--- src/main/java/pl/mdemski/model/GlazingType.java
+++ src/main/java/pl/mdemski/model/GlazingType.java
@@ -2,6 +2,8 @@ package pl.mdemski.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
 @Table(name = "glazing_types")
@@ -12,13 +14,17 @@ public class GlazingType extends AbstractEntity {
     private double height;
     private double width;
     private double ugValue;
+    private String description;
+    private byte[] picture;
 
-    public GlazingType(String name, String type, double height, double width, double ugValue) {
+    public GlazingType(String name, String type, double height, double width, double ugValue, String description, byte[] picture) {
         this.name = name;
         this.type = type;
         this.height = height;
         this.width = width;
         this.ugValue = ugValue;
+        this.description = description;
+        this.picture = picture;
     }
 
     public GlazingType() {
@@ -62,5 +68,43 @@ public class GlazingType extends AbstractEntity {
 
     public void setWidth(double width) {
         this.width = width;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        GlazingType that = (GlazingType) o;
+        return Double.compare(that.height, height) == 0 &&
+                Double.compare(that.width, width) == 0 &&
+                Double.compare(that.ugValue, ugValue) == 0 &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(type, that.type) &&
+                Objects.equals(description, that.description) &&
+                Arrays.equals(picture, that.picture);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(super.hashCode(), name, type, height, width, ugValue, description);
+        result = 31 * result + Arrays.hashCode(picture);
+        return result;
     }
 }
