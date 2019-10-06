0c31f98c8e748edff5ef61ebbae9ae837d3deb0c
mdemski <m.demski@outlook.com>
2019-10-06 22:10:13 +0200

    Added new fields, getters and setters and hashCode and equals methods.

--- src/main/java/pl/mdemski/model/MountingAngle.java
+++ src/main/java/pl/mdemski/model/MountingAngle.java
@@ -2,15 +2,20 @@ package pl.mdemski.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
 @Table(name = "mounting_angles")
 public class MountingAngle extends AbstractEntity {
     private String name;
+    private String description;
+    private byte[] picture;
 
-    public MountingAngle(String name) {
+    public MountingAngle(String name, String description, byte[] picture) {
         this.name = name;
+        this.description = description;
+        this.picture = picture;
     }
 
     public MountingAngle() {
@@ -22,5 +27,39 @@ public class MountingAngle extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MountingAngle that = (MountingAngle) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Arrays.equals(picture, that.picture);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(super.hashCode(), name, description);
+        result = 31 * result + Arrays.hashCode(picture);
+        return result;
     }
 }
