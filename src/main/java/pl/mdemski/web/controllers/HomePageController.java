9d81a6004cd6887c6fa907cdcb95a0e3746ad99a
mdemski <m.demski@outlook.com>
2019-10-06 12:39:04 +0200

    Added log in mappings

--- src/main/java/pl/mdemski/web/controllers/HomePageController.java
+++ src/main/java/pl/mdemski/web/controllers/HomePageController.java
@@ -2,11 +2,15 @@ package pl.mdemski.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mdemski.dto.UserLoginDTO;
 import pl.mdemski.model.User;
 import pl.mdemski.services.UserService;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -20,9 +24,29 @@ public class HomePageController {
     }
 
     @GetMapping
-    public String prepareHomePage(Model model, Principal principal){
-        User userName = userService.getUserByEmail(principal.getName());
-        model.addAttribute("userName", userName.getFirstName());
+    public String prepareHomePage(Model model, Principal principal) {
+        if (principal != null){
+            Long userId = userService.getUserByEmail(principal.getName()).getId();
+            model.addAttribute("userId", userId);
+        }
         return "index";
+    }
+
+    @GetMapping("/logowanie")
+    public String prepareLoginPage(Model model){
+        model.addAttribute("loginData", new UserLoginDTO());
+        return "login";
+    }
+
+    @PostMapping("/logowanie")
+    public String processLoginPage(@Valid UserLoginDTO userLoginDTO, BindingResult result){
+        if (!result.hasErrors()) {
+            User userName = userService.getUserByEmail(userLoginDTO.getEmail());
+            Long userId = userName.getId();
+            return "redirect:/moje-konto/" + userId;
+        } else {
+            return "login";
+        }
+
     }
 }
