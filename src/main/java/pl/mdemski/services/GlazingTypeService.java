82742945f4876cdb7c0d0b7f7f1324a22d79917f
mdemski <m.demski@outlook.com>
2019-10-06 21:48:15 +0200

    Added new fields

--- src/main/java/pl/mdemski/services/GlazingTypeService.java
+++ src/main/java/pl/mdemski/services/GlazingTypeService.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import pl.mdemski.dto.GlazingTypeDTO;
+import pl.mdemski.model.FileConverter;
 import pl.mdemski.model.GlazingType;
 import pl.mdemski.repositories.GlazingTypeRepository;
 
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,8 +26,10 @@ public class GlazingTypeService {
     }
 
     public void addGlazingType(){
-        GlazingType glazingType = new GlazingType("E2", "dwuszybowy", 700, 1060, 1.1);
-        GlazingType glazingType1 = new GlazingType("I3", "trzyszybowy", 700, 1060, 0.5);
+        byte[] doubleGlazing = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\dwuszybowy.png");
+        byte[] tripleGlazing = FileConverter.converter("C:\\PROGRAMOWANIE\\codersLab\\OrderingFoodApplication\\src\\main\\webapp\\resources\\static\\trzyszybowy.png");
+        GlazingType glazingType = new GlazingType("E1", "dwuszybowy", 700, 1060, 1.1, "Pakiet dwuszybowy bez zewnętrznie hartowanej szyby", doubleGlazing);
+        GlazingType glazingType1 = new GlazingType("I22", "trzyszybowy", 700, 1060, 0.5, "Pakiet trzyszybowy z zewnętrznie hartowanej szybą zewnętrzną", tripleGlazing);
         glazingTypeRepository.save(glazingType);
         glazingTypeRepository.save(glazingType1);
     }
