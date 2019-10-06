d6ef0de5f3a5ed71ca7c9d7d10221272e1bd8b19
mdemski <m.demski@outlook.com>
2019-10-06 22:27:41 +0200

    Added new fields, getters and setters and changed hashCode and equals methods.

--- src/main/java/pl/mdemski/model/Window.java
+++ src/main/java/pl/mdemski/model/Window.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
@@ -30,8 +31,12 @@ public class Window extends AbstractEntity {
     @ManyToOne
     private User user;
     private LocalDateTime created;
+    private String description;
+    private byte[] picture;
+    private double price;
+    private boolean priceList;
 
-    public Window(MountingAngle mountingAngle, Material material, OpeningType openingType, int height, int width, GlazingType glazingType, MaterialColor materialColor, Handle handle, Flashing flashing, Ventilator ventilator, User user, LocalDateTime created) {
+    public Window(MountingAngle mountingAngle, Material material, OpeningType openingType, int height, int width, GlazingType glazingType, MaterialColor materialColor, Handle handle, Flashing flashing, Ventilator ventilator, User user, LocalDateTime created, String description, byte[] picture, double price, boolean priceList) {
         this.mountingAngle = mountingAngle;
         this.material = material;
         this.openingType = openingType;
@@ -44,6 +49,10 @@ public class Window extends AbstractEntity {
         this.ventilator = ventilator;
         this.user = user;
         this.created = created;
+        this.description = description;
+        this.picture = picture;
+        this.price = price;
+        this.priceList = priceList;
     }
 
     public Window() {
@@ -145,15 +154,48 @@ public class Window extends AbstractEntity {
         this.created = created;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(boolean priceList) {
+        this.priceList = priceList;
+    }
+
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Window window = (Window) o;
         return height == window.height &&
                 width == window.width &&
+                Double.compare(window.price, price) == 0 &&
+                priceList == window.priceList &&
                 Objects.equals(mountingAngle, window.mountingAngle) &&
                 Objects.equals(material, window.material) &&
                 Objects.equals(openingType, window.openingType) &&
@@ -163,11 +205,15 @@ public class Window extends AbstractEntity {
                 Objects.equals(flashing, window.flashing) &&
                 Objects.equals(ventilator, window.ventilator) &&
                 Objects.equals(user, window.user) &&
-                Objects.equals(created, window.created);
+                Objects.equals(created, window.created) &&
+                Objects.equals(description, window.description) &&
+                Arrays.equals(picture, window.picture);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), mountingAngle, material, openingType, height, width, glazingType, materialColor, handle, flashing, ventilator, user, created);
+        int result = Objects.hash(super.hashCode(), mountingAngle, material, openingType, height, width, glazingType, materialColor, handle, flashing, ventilator, user, created, description, price, priceList);
+        result = 31 * result + Arrays.hashCode(picture);
+        return result;
     }
 }
