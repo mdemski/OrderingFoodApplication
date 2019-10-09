4a427ce22ead35fe6a7f489aa075b9f812345e14
mdemski <m.demski@outlook.com>
2019-10-09 18:18:45 +0200

    Finding by price list condition method added

--- src/main/java/pl/mdemski/repositories/WindowRepository.java
+++ src/main/java/pl/mdemski/repositories/WindowRepository.java
@@ -1,5 +1,7 @@
 package pl.mdemski.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.mdemski.model.AbstractEntity;
 import pl.mdemski.model.Window;
@@ -8,4 +10,5 @@ public interface WindowRepository<T extends AbstractEntity, L extends Number> ex
 
     Window findById(Long id);
 
+    Page<Window> findByPriceList(PageRequest pageRequest, boolean priceList);
 }
