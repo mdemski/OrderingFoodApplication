84da611624a296f206bb5ec12c20b5a4b1b1b09a
mdemski <m.demski@outlook.com>
2019-10-09 21:14:46 +0200

    Rename and fix method

--- src/main/java/pl/mdemski/repositories/WindowRepository.java
+++ src/main/java/pl/mdemski/repositories/WindowRepository.java
@@ -1,14 +1,15 @@
 package pl.mdemski.repositories;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.mdemski.model.AbstractEntity;
 import pl.mdemski.model.Window;
+
 
 public interface WindowRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Window, Long> {
 
     Window findById(Long id);
 
-    Page<Window> findByPriceList(PageRequest pageRequest, boolean priceList);
+    Page<Window> findAllByPriceList(Pageable pageable, boolean priceList);
 }
